9bf1d62821b0d77c6ebb161fdca61203c00eabc0
teabaggs <rowanphil@gmail.com>
2014-06-24 20:26:29 -0500

    Revert "adds basic listener support"
    This reverts commit 12409432f0c4c22da0fa60a1ec33b24b096740bf.

--- Source/Topshelf.Quartz/QuartzConfigurator.cs
+++ Source/Topshelf.Quartz/QuartzConfigurator.cs
@@ -4,79 +4,33 @@ using Quartz;
 
 namespace Topshelf.Quartz
 {
-    public class QuartzJobListenerConfig
-    {
-        public QuartzJobListenerConfig(IJobListener listener, params IMatcher<JobKey>[] matchers)
-        {
-            Listener = listener;
-            Matchers = matchers;
-        }
-        public IJobListener Listener { get; set; }
-        public IList<IMatcher<JobKey>> Matchers { get; set; }
-    }
+	public class QuartzConfigurator
+	{
+		public Func<IJobDetail> Job { get; set; }
+		public IList<Func<ITrigger>> Triggers { get; set; }
+		public Func<bool> JobEnabled { get; set; }
 
-    public class QuartzTriggerListenerConfig
-    {
-        public QuartzTriggerListenerConfig(ITriggerListener listener, params IMatcher<TriggerKey>[] matchers)
-        {
-            Listener = listener;
-            Matchers = matchers;
-        }
-        public ITriggerListener Listener { get; set; }
-        public IList<IMatcher<TriggerKey>> Matchers { get; set; }
-    }
+		public QuartzConfigurator()
+		{
+			Triggers = new List<Func<ITrigger>>();
+		}
 
-    public class QuartzConfigurator
-    {
-        public Func<IJobDetail> Job { get; set; }
-        public IList<Func<ITrigger>> Triggers { get; set; }
-        public IList<Func<QuartzJobListenerConfig>> JobListeners { get; set; }
-        public IList<Func<QuartzTriggerListenerConfig>> TriggerListeners { get; set; }
-        public IList<Func<ISchedulerListener>> ScheduleListeners { get; set; }
-        public Func<bool> JobEnabled { get; set; }
+		public QuartzConfigurator WithJob(Func<IJobDetail> jobDetail)
+		{
+			Job = jobDetail;
+			return this;
+		}
 
-        public QuartzConfigurator()
-        {
-            Triggers = new List<Func<ITrigger>>();
-            TriggerListeners = new List<Func<QuartzTriggerListenerConfig>>();
-            JobListeners = new List<Func<QuartzJobListenerConfig>>();
-            ScheduleListeners = new List<Func<ISchedulerListener>>();
-        }
+		public QuartzConfigurator AddTrigger(Func<ITrigger> jobTrigger)
+		{
+			Triggers.Add(jobTrigger);
+			return this;
+		}
 
-        public QuartzConfigurator WithJob(Func<IJobDetail> jobDetail)
-        {
-            Job = jobDetail;
-            return this;
-        }
-
-        public QuartzConfigurator AddTrigger(Func<ITrigger> jobTrigger)
-        {
-            Triggers.Add(jobTrigger);
-            return this;
-        }
-
-        public QuartzConfigurator EnableJobWhen(Func<bool> jobEnabled)
-        {
-            JobEnabled = jobEnabled;
-            return this;
-        }
-
-        public QuartzConfigurator WithJobListener(Func<QuartzJobListenerConfig> jobListener)
-        {
-            JobListeners.Add(jobListener);
-            return this;
-        }
-
-        public QuartzConfigurator WithTriggerListener(Func<QuartzTriggerListenerConfig> triggerListener)
-        {
-            TriggerListeners.Add(triggerListener);
-            return this;
-        }
-
-        public QuartzConfigurator WithScheduleListener(Func<ISchedulerListener> scheduleListener)
-        {
-            ScheduleListeners.Add(scheduleListener);
-            return this;
-        }
-    }
+		public QuartzConfigurator EnableJobWhen(Func<bool> jobEnabled)
+		{
+			JobEnabled = jobEnabled;
+			return this;
+		}
+	}
 }
--- Source/Topshelf.Quartz/SchedulejobServiceConfiguratorExtensions.cs
+++ Source/Topshelf.Quartz/SchedulejobServiceConfiguratorExtensions.cs
@@ -9,138 +9,95 @@ using Topshelf.ServiceConfigurators;
 
 namespace Topshelf.Quartz
 {
-    public static class ScheduleJobServiceConfiguratorExtensions
-    {
-        private static readonly Func<IScheduler> DefaultSchedulerFactory = () =>
-                                                                               {
-                                                                                   var schedulerFactory = new StdSchedulerFactory();
-                                                                                   return schedulerFactory.GetScheduler();
-                                                                               };
+	public static class ScheduleJobServiceConfiguratorExtensions
+	{
+		private static readonly Func<IScheduler> DefaultSchedulerFactory = () =>
+			                                                                   {
+				                                                                   var schedulerFactory = new StdSchedulerFactory();
+				                                                                   return schedulerFactory.GetScheduler();
+			                                                                   };
 
-        private static Func<IScheduler> _customSchedulerFactory;
-        private static IScheduler Scheduler;
-        internal static IJobFactory JobFactory;
+		private static Func<IScheduler> _customSchedulerFactory;
+		private static IScheduler Scheduler;
+		internal static IJobFactory JobFactory;
 
-        public static Func<IScheduler> SchedulerFactory
-        {
-            get { return _customSchedulerFactory ?? DefaultSchedulerFactory; }
-            set { _customSchedulerFactory = value; }
-        }
+		public static Func<IScheduler> SchedulerFactory
+		{
+			get { return _customSchedulerFactory ?? DefaultSchedulerFactory; }
+			set { _customSchedulerFactory = value; }
+		}
 
-        private static IScheduler GetScheduler()
-        {
-            var scheduler = SchedulerFactory();
+		private static IScheduler GetScheduler()
+		{
+			var scheduler = SchedulerFactory();
+			
+			if(JobFactory != null)
+				scheduler.JobFactory = JobFactory;
 
-            if (JobFactory != null)
-                scheduler.JobFactory = JobFactory;
+			return scheduler;
+		}
 
-            return scheduler;
-        }
+		public static ServiceConfigurator<T> UsingQuartzJobFactory<T, TJobFactory>(this ServiceConfigurator<T> configurator, Func<TJobFactory> jobFactory)
+			where T : class
+			where TJobFactory : IJobFactory
+		{
+			JobFactory = jobFactory();
+			return configurator;
+		}
 
-        public static ServiceConfigurator<T> UsingQuartzJobFactory<T, TJobFactory>(this ServiceConfigurator<T> configurator, Func<TJobFactory> jobFactory)
-            where T : class
-            where TJobFactory : IJobFactory
-        {
-            JobFactory = jobFactory();
-            return configurator;
-        }
+		public static ServiceConfigurator<T> UsingQuartzJobFactory<T,TJobFactory>(this ServiceConfigurator<T> configurator) where T : class where TJobFactory : IJobFactory, new()
+		{
+			return UsingQuartzJobFactory(configurator, () => new TJobFactory());
+		}
 
-        public static ServiceConfigurator<T> UsingQuartzJobFactory<T, TJobFactory>(this ServiceConfigurator<T> configurator)
-            where T : class
-            where TJobFactory : IJobFactory, new()
-        {
-            return UsingQuartzJobFactory(configurator, () => new TJobFactory());
-        }
+		public static ServiceConfigurator<T> ScheduleQuartzJob<T>(this ServiceConfigurator<T> configurator, Action<QuartzConfigurator> jobConfigurator) where T : class
+		{
+			ConfigureJob<T>(configurator, jobConfigurator);
+			return configurator;
+		}
 
-        public static ServiceConfigurator<T> ScheduleQuartzJob<T>(this ServiceConfigurator<T> configurator, Action<QuartzConfigurator> jobConfigurator) where T : class
-        {
-            ConfigureJob<T>(configurator, jobConfigurator);
-            return configurator;
-        }
+		private static void ConfigureJob<T>(ServiceConfigurator<T> configurator, Action<QuartzConfigurator> jobConfigurator) where T : class
+		{
+			var log = HostLogger.Get(typeof(ScheduleJobServiceConfiguratorExtensions));
 
-        private static void ConfigureJob<T>(ServiceConfigurator<T> configurator, Action<QuartzConfigurator> jobConfigurator) where T : class
-        {
-            var log = HostLogger.Get(typeof(ScheduleJobServiceConfiguratorExtensions));
+			var jobConfig = new QuartzConfigurator();
+			jobConfigurator(jobConfig);
 
-            var jobConfig = new QuartzConfigurator();
-            jobConfigurator(jobConfig);
+			if (jobConfig.JobEnabled == null || jobConfig.JobEnabled() || (jobConfig.Job == null || jobConfig.Triggers == null))
+			{
+				var jobDetail = jobConfig.Job();
+				var jobTriggers = jobConfig.Triggers.Select(triggerFactory => triggerFactory()).Where(trigger => trigger != null);
 
-            if (jobConfig.JobEnabled == null || jobConfig.JobEnabled() || (jobConfig.Job == null || jobConfig.Triggers == null))
-            {
-                var jobDetail = jobConfig.Job();
-                var jobTriggers = jobConfig.Triggers.Select(triggerFactory => triggerFactory()).Where(trigger => trigger != null);
-                var jobListeners = jobConfig.JobListeners;
-                var triggerListeners = jobConfig.TriggerListeners;
-                var scheduleListeners = jobConfig.ScheduleListeners;
-                configurator.BeforeStartingService(() =>
-                                                       {
-                                                           log.Debug("[Topshelf.Quartz] Scheduler starting up...");
-                                                           if (Scheduler == null)
-                                                               Scheduler = GetScheduler();
+				configurator.BeforeStartingService(() =>
+					                                   {
+														   log.Debug("[Topshelf.Quartz] Scheduler starting up...");
+														   if (Scheduler == null)
+															   Scheduler = GetScheduler();
+														   
+														   
+															if (Scheduler != null && jobDetail != null && jobTriggers.Any())
+															{
+																var triggersForJob = new HashSet<ITrigger>(jobTriggers);
+																Scheduler.ScheduleJob(jobDetail, triggersForJob, false);
+																log.Info(string.Format("[Topshelf.Quartz] Scheduled Job: {0}", jobDetail.Key));
+					
+																foreach(var trigger in triggersForJob)
+																	log.Info(string.Format("[Topshelf.Quartz] Job Schedule: {0} - Next Fire Time (local): {1}", trigger, trigger.GetNextFireTimeUtc().HasValue ? trigger.GetNextFireTimeUtc().Value.ToLocalTime().ToString() : "none"));
 
+																Scheduler.Start();
+																log.Info("[Topshelf.Quartz] Scheduler started...");
+															}
 
-                                                           if (Scheduler != null && jobDetail != null && jobTriggers.Any())
-                                                           {
-                                                               var triggersForJob = new HashSet<ITrigger>(jobTriggers);
-                                                               Scheduler.ScheduleJob(jobDetail, triggersForJob, false);
-                                                               log.Info(string.Format("[Topshelf.Quartz] Scheduled Job: {0}", jobDetail.Key));
+					                                   });
 
-                                                               foreach (var trigger in triggersForJob)
-                                                                   log.Info(string.Format("[Topshelf.Quartz] Job Schedule: {0} - Next Fire Time (local): {1}", trigger, trigger.GetNextFireTimeUtc().HasValue ? trigger.GetNextFireTimeUtc().Value.ToLocalTime().ToString() : "none"));
+				configurator.BeforeStoppingService(() =>
+						                {
+											log.Debug("[Topshelf.Quartz] Scheduler shutting down...");
+											Scheduler.Shutdown(true);
+											log.Info("[Topshelf.Quartz] Scheduler shut down...");
+						                });
 
-
-                                                               if (jobListeners.Any())
-                                                               {
-                                                                   foreach (var listener in jobListeners)
-                                                                   {
-                                                                       var config = listener();
-                                                                       Scheduler.ListenerManager.AddJobListener(
-                                                                           config.Listener, config.Matchers);
-                                                                       log.Info(
-                                                                           string.Format(
-                                                                               "[Topshelf.Quartz] Added Job Listener: {0}",
-                                                                               config.Listener.Name));
-                                                                   }
-                                                               }
-                                                               if (triggerListeners.Any())
-                                                               {
-                                                                   foreach (var listener in triggerListeners)
-                                                                   {
-                                                                       var config = listener();
-                                                                       Scheduler.ListenerManager.AddTriggerListener(config.Listener, config.Matchers);
-                                                                       log.Info(
-                                                                       string.Format(
-                                                                           "[Topshelf.Quartz] Added Trigger Listener: {0}",
-                                                                           config.Listener.Name));
-                                                                   }
-                                                               }
-                                                               if (scheduleListeners.Any())
-                                                               {
-                                                                   foreach (var listener in scheduleListeners)
-                                                                   {
-                                                                       var schedListener = listener();
-                                                                       Scheduler.ListenerManager.AddSchedulerListener(schedListener);
-                                                                       string.Format(
-                                                                           "[Topshelf.Quartz] Added Schedule Listener: {0}",
-                                                                           schedListener.GetType());
-                                                                   }
-                                                                   
-                                                               }
-                                                               
-                                                               Scheduler.Start();
-                                                               log.Info("[Topshelf.Quartz] Scheduler started...");
-
-                                                           }
-                                                       });
-
-                configurator.BeforeStoppingService(() =>
-                                        {
-                                            log.Debug("[Topshelf.Quartz] Scheduler shutting down...");
-                                            Scheduler.Shutdown(true);
-                                            log.Info("[Topshelf.Quartz] Scheduler shut down...");
-                                        });
-
-            }
-        }
-    }
+			}
+		}
+	}
 }
