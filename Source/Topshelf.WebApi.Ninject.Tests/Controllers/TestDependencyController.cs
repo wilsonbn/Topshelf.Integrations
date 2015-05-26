1bb3ba5111748caf42cd5707ab8ba714ac25cbfe
markhuber <markhuber86@gmail.com>
2015-05-25 22:04:14 -0500

    Added test suite for Topshelf.Integrations

---
+++ Source/Topshelf.WebApi.Ninject.Tests/Controllers/TestDependencyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using Topshelf.Common.Tests;
+
+namespace Topshelf.WebApi.Ninject.Tests.Controllers
+{
+    public class TestDependencyController : ApiController
+    {
+        private readonly SampleDependency _dependency;
+
+        public TestDependencyController(SampleDependency dependency)
+        {
+            _dependency = dependency;
+        }
+
+        [HttpGet]
+        public int Get()
+        {
+            if(_dependency != null)
+                return 42;
+            return 0;
+        }
+    }
+}
